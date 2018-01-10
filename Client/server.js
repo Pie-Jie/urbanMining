3d5dee7afe450bc025772f9d545127022c6cb8a1
Pie-Jie <pie.jie@hotmail.com>
2018-01-10 18:14:33 +0100

    Client updated

--- Client/server.js
+++ Client/server.js
@@ -1,15 +1,13 @@
-'use strict';
 var Particle = require('particle-api-js');
 var particle = new Particle();
-var token = '0e93f7f15227ab29cad4b378f89a61e92ad86360';
-var devicesPr = particle.listDevices({ auth: token });
-var deviceID = '2f0053000b51343136333035';
-var dataFromParticle;
-
+var fs = require('fs');
+var token;
 var devicesPr = particle.listDevices({ auth: token });
 
-
-
+var osc = require("osc");
+const path = require('path');
+const PORT = process.env.PORT || 4000;
+const INDEX = path.join(__dirname, 'index.html');
 
 const express = require('express');
 var io = require('socket.io-client');
@@ -17,84 +15,89 @@ var socket = io.connect('ws://afternoon-shore-70838.herokuapp.com', {
     reconnect: true
 });
 
-var osc = require("osc");
-const path = require('path');
-const PORT = process.env.PORT || 4000;
-const INDEX = path.join(__dirname, 'index.html');
-
 const server = express()
     .use((req, res) => res.sendFile(INDEX))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-
-// SOCKET IO Add a connect listener
-socket.on('connect', function(socket) {
+socket.on('connect', function (socket) {
     console.log('Connected!');
 });
 
+particle.login({
+    username: 'bram.speeckaert@student.ehb.be',
+    password: 'finalWork'
+}).then(
+    function (data) {
+        token = data.body.access_token;
+        console.log('API call completed on promise resolve: ', token);
+        startStream();
+    },
+    function (err) {
+        console.log('Could not log in.', err);
+    }
+);
 
-/*
-particle.getEventStream({ auth: token }).then(function(stream) {
-    
-    stream.on('gpsdata', function(data) {
-        var interval = setInterval(function() {
-        currentTime += waitInterval;
-        percentWaited = Math.floor(currentTime);
-        writeWaitingPercent(percentWaited);
-        socket.emit('/test', (currentTime / 100));
-        }, waitInterval);      
-        var incomingData = data.data.split('/');
-        console.log(`latitude ${incomingData[0]}, altitude ${incomingData[1]}, longitude ${incomingData[2]}`);
-        
-        //socket.emit('/latitude', incomingData[0]);    
-        //socket.emit('/altitude', incomingData[1]);    
-        //socket.emit('/longitude', incomingData[2]);    
-        
+
+var startStream = function () {
+    var devicesPr = particle.getEventStream({
+        name: 'gpsData',
+        auth: token
     });
-        
-
-
-});
-
-*/
-socket.on('settings', function(data){
-  const settings = data;
-  console.log("received settings :: " + data);
-
-  var udpPort = new osc.UDPPort({
-      localAddress: "0.0.0.0",
-      localPort: settings.oscInputPort
-  });
-
-  //send osc message to server
-    /*
-    particle.getEventStream({ auth: token }).then(function(stream) {
-    stream.on('longitude', function(data) {
-      console.log("Event: ", data);
-      socket.emit(`/${data.name}`, data.data)
-  });
-});
-*/
+    devicesPr.then(function (stream) {
+        stream.on('event', function (data) {
+            var incomingData = data.data.split('/');
+            var obj = {
+                synfo: []
+            };
+            obj.synfo.push({
+                latitude: incomingData[0],
+                longitude: incomingData[1],
+                altitude: incomingData[2],
+                kmph: incomingData[3]
+            });
+            var json = JSON.stringify(obj);
+            fs.appendFile('data.json', json, 'UTF-8', function (err) {
+                if (err) {
+                    console.log(`Error while appending data to json file ${err}`);
+                }
+            });
+            socket.emit('/latitude', incomingData[0]);
+            socket.emit('/longitude', incomingData[1]);
+            socket.emit('/altitude', incomingData[2]);
+            socket.emot('/kmph', incomingData[3]);
+        });
+                
     
-  udpPort.on("message", function(oscMsg) {
-      //oscMsg = dataFromParticle;
-      console.log(`osc msg: ${oscMsg.address}`);
-      console.log(`osc args: ${oscMsg.args}`);
-      socket.emit(oscMsg.address, oscMsg.args)
-  });
-
-  for (let param of settings.params) {
-    socket.on(param, (data) => {
-        udpPort.send({
-            address: param,
-            args: data
-        }, "127.0.0.1", settings.oscOutputPort);
+        stream.on('end', function () {
+            console.warn("Event stream ended! re-opening in 1 second...");
+            setTimeout(openStream, 1 * 1000);
+        });
+    
     });
-  }
+};
 
-  // Open the socket.
-  udpPort.open();
+socket.on('settings', function (data) {
+    const settings = data;
     
+    var udpPort = new osc.UDPPort({
+        localAddress: "0.0.0.0",
+        localPort: settings.oscInputPort
+    });
+
+    udpPort.on("message", function (oscMsg) {
+        console.log(`osc msg: ${oscMsg.address}`);
+        console.log(`osc args: ${oscMsg.args} with type: ${typeof oscMsg.args}`);
+        socket.emit(oscMsg.address, oscMsg.args)
+    });
+
+    for (let param of settings.params) {
+        socket.on(param, (data) => {
+            udpPort.send({
+                address: param,
+                args: parseFloat(data)
+            }, "127.0.0.1", settings.oscOutputPort);
+        });
+    }
+    udpPort.open();
+
 });
-
-
